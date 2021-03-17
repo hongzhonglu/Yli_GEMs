247c7ab0008c7699eb80bc70030d11283db73b9e
hongzhonglu <hongzhong.lu@wlab.gu.se>
2021-03-17 16:57:56 +0100

    feat: add new calculation based on the iYali_v4

--- code/iYLI647.py
+++ code/iYLI647.py
@@ -12,12 +12,8 @@ import sys
 import pprint
 import pandas as pd
 
-os.chdir('/Users/luho/PycharmProjects/model/cobrapy/code')
-sys.path.append(r"/Users/luho/PycharmProjects/model/cobrapy/code")
-pprint.pprint(sys.path)
 # model = read_sbml_model("../data/iYLI647_update2.xml") # can't read xml file now
-model = load_matlab_model("/Users/luho/PycharmProjects/model/cobrapy/data/iYLI647_update2.mat")
-
+model = load_matlab_model("model/iYLI647_update2.mat")
 
 # nitrogen limitation
 model.objective = 'R_r156'
@@ -27,4 +23,27 @@ model.reactions.get_by_id("R_EX_o2(e)").bounds = (-2,0)
 solution = model.optimize()
 print(model.summary())
 fluxes = pd.DataFrame(solution.fluxes)
-fluxes.to_csv("../result/iYLI647.csv")
+fluxes.to_csv("result/iYLI647.csv")
+
+# test using the new data from Fujing
+model = load_matlab_model("model/iYLI647_update2.mat")
+# strain 1
+model.reactions.get_by_id("R_EX_glc(e)").bounds = (-1.04, 0)
+#model.reactions.get_by_id("R_EX_o2(e)").bounds = (-2,0) # current no data
+model.reactions.get_by_id("R_EX_co2(e)").bounds = (2.06, 2.06)
+solution2 = model.optimize()
+
+# strain 2
+model.reactions.get_by_id("R_EX_glc(e)").bounds = (-1.237, 0)
+#model.reactions.get_by_id("R_EX_o2(e)").bounds = (-2,0) # current no data
+model.reactions.get_by_id("R_EX_co2(e)").bounds = (3.00, 3.00)
+solution2 = model.optimize()
+
+
+# test using Ed data
+model = load_matlab_model("model/iYLI647_update2.mat")
+model.reactions.get_by_id("R_EX_glc(e)").bounds = (-0.61, 0)
+#model.reactions.get_by_id("R_EX_o2(e)").bounds = (-2,0) # current no data
+model.reactions.get_by_id("R_EX_co2(e)").bounds = (1.5, 1.5)
+solution3 = model.optimize()
+
